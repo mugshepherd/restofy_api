7084701c97493262097a36947954f6aa76a1d544
Toby <toby.schaeffer@gmail.com>
2015-11-14 17:20:08 +0300

    added comments, reviewed code

--- js/models/allRestaurants.js
+++ js/models/allRestaurants.js
@@ -1,7 +1,10 @@
 var AllRestaurants = function(){
-  this.restaurants = [] // not needed b/c it's assigned, but doesn't really hurt for clarity
+  this.restaurants = [] 
+  // ts - When you say that it's 'assigned,' do you mean the brackets aren't necessary, or that it's not necessary to declare the variable?   
+  //// not needed b/c it's assigned, but doesn't really hurt for clarity
   this.fetchRestaurants()
-  // return this.restaurants // I believe you want to strike this line. Otherwise you can never get an instance of the class.
+  // ts - i don't understand this (that i won't be able to get an instance of the class)).  
+  //// return this.restaurants // I believe you want to strike this line. Otherwise you can never get an instance of the class.
 }
 
 AllRestaurants.prototype = {
@@ -34,18 +37,21 @@ AllRestaurants.prototype = {
   },
   
   loadRestaurants: function(response) {
-    // In js, vars are scoped by function. Make sure to define all vars at the top of the body of a function.
-    // This makes it clear what you're defining and prevents hoisting.
-    // See eg https://javascriptweblog.wordpress.com/2010/07/06/function-declarations-vs-function-expressions/
+    //ts: very helpful link.  read up on scoping and hoisting.  thank you.  
+    //// In js, vars are scoped by function. Make sure to define all vars at the top of the body of a function.
+    //// This makes it clear what you're defining and prevents hoisting.
+    //// See eg https://javascriptweblog.wordpress.com/2010/07/06/function-declarations-vs-function-expressions/
 
-    var i, restaurant;
+    var i, restaurant;  
     this.restaurants = [];
     for(i = 0; i < response.length; i++){
       restaurant = new Restaurant(response[i].pid, response[i].addr, response[i].name);
       this.restaurants.push(restaurant);
     }
 
-    // Better yet, use this functional style below to make the purpose of your code clearer
+    //ts:  i really like this - very helpful to make sure I undestand what's going on.  will do this moving forward.  
+    //// Better yet, use this functional style below to make the purpose of your code clearer
+    
     /*
     this.restaurants = response.map(function(item) {
       return new Restaurant(item.pid, item.addr, item.name);
@@ -56,10 +62,18 @@ AllRestaurants.prototype = {
   loadDetails: function(pid, response) {
     // same comments on var definition and functional style as for the above loadRestaurants.
 
+    var i, restaurant_detailed;
     restaurant_detailed = []
     for(var i = 0; i < response.length; i++){
-      var restaurant_detailed = Restaurant[pid](response[i].coords, response[i].totals, response[i].violations);
+      restaurant_detailed = Restaurant[pid](response[i].coords, response[i].totals, response[i].violations);
       this.restaurants.push(restaurant_detailed);
     }
+
+    //ts:  Yoshi, can you please confirm I've done this right?  
+    /*
+    this.restaurant_detailed = reponse.map(function(item){
+      return restaurant[pid](item.coords, item.totals, item.violatios);
+    });
+    */
   }
 }
